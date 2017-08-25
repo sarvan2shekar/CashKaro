4f3746748fd823da30bca7e56dabce84ed8b376e
Saravan Sekar Thillai <sarvan2shekar@gmail.com>
2017-08-26 03:09:41 +0530

    Implemented left menu in home page

--- app/src/main/java/com/testapp/sarvan/cashkaro/view/HomeActivity.java
+++ app/src/main/java/com/testapp/sarvan/cashkaro/view/HomeActivity.java
@@ -1,22 +1,43 @@
 package com.testapp.sarvan.cashkaro.view;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.testapp.sarvan.cashkaro.CarouselPagerAdapter;
 import com.testapp.sarvan.cashkaro.R;
 
 public class HomeActivity extends AppCompatActivity {
-
     public final static int LOOPS = 1000;
     public static int count = 6; //ViewPager items size should be dynamically got from CashKaro Web services
     public static int FIRST_PAGE = 6;
     private final int backDuration = 2000;
+    private final int MY_PERMISSIONS_REQUEST_CAMERA = 998;
+    private final int MY_PERMISSIONS_REQUEST_LOCATION = 999;
     public ViewPager pager;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    private String[] mMenuTitles;
     private CarouselPagerAdapter adapter;
     private boolean doubleBackToExitPressedOnce = false;
 
@@ -24,12 +45,13 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(getString(R.string.home_title));
+        }
+
         pager = (ViewPager) findViewById(R.id.dealsCarousel);
-
         DisplayMetrics metrics = new DisplayMetrics();
-
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
-
         int pageMargin = ((metrics.widthPixels / 4) * 2);
         pager.setPageMargin(-pageMargin);
         adapter = new CarouselPagerAdapter(this, getSupportFragmentManager());
@@ -38,9 +60,53 @@ public class HomeActivity extends AppCompatActivity {
         pager.addOnPageChangeListener(adapter);
         pager.setCurrentItem(FIRST_PAGE);
         pager.setOffscreenPageLimit(3);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setTitle(getString(R.string.home_title));
-        }
+
+        // enable ActionBar app icon to behave as action to toggle nav drawer
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        mTitle = mDrawerTitle = getSupportActionBar().getTitle();
+        mMenuTitles = getResources().getStringArray(R.array.menuVals);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.leftMenu);
+        mDrawerList = (ListView) findViewById(R.id.navList);
+
+        // set a custom shadow that overlays the main content when the drawer opens
+        //mDrawerLayout.setDrawerShadow(android.R.drawable.drawer_shadow, GravityCompat.START);
+        // set up the drawer's list view with items and click listener
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mMenuTitles));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+
+        // ActionBarDrawerToggle ties together the the proper interactions
+        // between the sliding drawer and the action bar app icon
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                //R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description for accessibility */
+                R.string.drawer_close  /* "close drawer" description for accessibility */
+        ) {
+            public void onDrawerClosed(View view) {
+                getSupportActionBar().setTitle(mTitle);
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle(mDrawerTitle);
+            }
+        };
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
     @Override
@@ -60,5 +126,88 @@ public class HomeActivity extends AppCompatActivity {
                 doubleBackToExitPressedOnce = false;
             }
         }, backDuration);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return mDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+
+    public boolean getPermission(String permission, int requestCode) {
+        if (ContextCompat.checkSelfPermission(HomeActivity.this,
+                permission) != PackageManager.PERMISSION_GRANTED) {
+            System.out.println("Test permission");
+            // Should we show an explanation?
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(HomeActivity.this,
+//                    permission)) {
+//                //TODO implement snackbar
+//                // Show an explanation to the user *asynchronously* -- don't block
+//                // this thread waiting for the user's response! After the user
+//                // sees the explanation, try again to request the permission.
+//
+//
+//            } else {
+            ActivityCompat.requestPermissions(HomeActivity.this,
+                    new String[]{permission},
+                    requestCode);
+//            }
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[],
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                break;
+            }
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                break;
+            }
+        }
+    }
+
+    private class DrawerItemClickListener implements android.widget.AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+            String selectedVal = (adapterView.getItemAtPosition(position)).toString();
+            System.out.println("Item clicked: " +
+                    selectedVal);
+            if (selectedVal.trim().equalsIgnoreCase(getString(R.string.camera))) {
+                if (getPermission(Manifest.permission.CAMERA, MY_PERMISSIONS_REQUEST_CAMERA)) {
+                    Snackbar.make(view, "Permission already granted.", Snackbar.LENGTH_LONG).show();
+                } else {
+                    Snackbar.make(view, "Please request permission.", Snackbar.LENGTH_LONG).show();
+                }
+            } else if (selectedVal.trim().equalsIgnoreCase(getString(R.string.location))) {
+                if (getPermission(Manifest.permission.ACCESS_COARSE_LOCATION,
+                        MY_PERMISSIONS_REQUEST_LOCATION)) {
+                    Snackbar.make(view, "Permission already granted.", Snackbar.LENGTH_LONG).show();
+                } else {
+                    Snackbar.make(view, "Please request permission.", Snackbar.LENGTH_LONG).show();
+                }
+            }
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        }
     }
 }
